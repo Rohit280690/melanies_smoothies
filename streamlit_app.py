fbc52015c3582b1f5d6a0868ca6d2f6c53d7cd15
Rohit280690 <rohit.gokhale90@gmail.com>
2024-05-25 15:22:40 +0530

    Update streamlit_app.py
    Added requests code

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,10 +50,11 @@ if ingredients_list:
     values('""" + ingredients_string + """','""" + name_on_order + """')"""
     #st.write(my_insert_stmt)
     #st.stop()
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)   
+  
     time_to_insert = st.button("Submit Order")
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         #st.success('Your Smoothie is ordered,',name_on_order,'!', icon="✅")
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
