6a65de4c36f359eaa6ee3910e84d765e16c986ee
Rohit280690 <rohit.gokhale90@gmail.com>
2024-05-25 14:58:02 +0530

    Update streamlit_app.py
    changed to cnx.session()

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,8 @@ st.write('The name on smoothie will be',name_on_order)
 #st.write('You favorite fruit is:', option)
 
 cnx=st.connection("snowflake")
-session = cnx.connection()
+#session = cnx.connection()
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
